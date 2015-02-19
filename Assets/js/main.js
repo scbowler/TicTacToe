3354681bd8dc3c4e2dd170e10f5a304f7c98291c
Scott Bowler <s.bowler1007@gmail.com>
2015-02-19 15:13:41 -0800

    JS update3 Scott

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,29 +1,32 @@
 var boardArray, playerX, playerO, ties, whosTurn;
 
 function init(){
-    var boardArray = [['', '', ''], ['', '', ''], ['', '', '']];
-    var playerX = ["X", 0];
-    var playerO = ["O", 0];
-    var ties = 0;
-    var whosTurn = "";
+    boardArray = [['', '', ''], ['', '', ''], ['', '', '']];
+    playerX = ["X", 0];
+    playerO = ["O", 0];
+    ties = 0;
+    whosTurn = "";
 }
+
 
 function didWin(player){
     var arrLength = boardArray.length;
     
-    if(boardArray[0][0] == player && boardArray[1][1] == player && boardArray[2][2] == player ||
-       boardArray[0][2] == player && boardArray[1][1] == player && boardArray[2][0] == player){
-        return true;
+    if(boardArray[0][0] == player && boardArray[1][1] == player && boardArray[2][2] == player){
+        return console.log("\\");
+    }
+    if(boardArray[0][2] == player && boardArray[1][1] == player && boardArray[2][0] == player){
+        return console.log("/");
     }
     
     for(var i=0; i<arrLength; i++){
         if(boardArray[i][0] == player && boardArray[i][1] == player && boardArray[i][2] == player ){
-            return true;
+            return console.log("r" + i);
         }else if(boardArray[0][i] == player && boardArray[1][i] == player && boardArray[2][i] == player ){
-            return true;    
+            return console.log("c" + i);    
         }
     }
-    return false;
+    return console.log(false);
 }
 
 function updateScore(player){
@@ -42,4 +45,4 @@ function updateScore(player){
                 alert("There was an error in the updateScore function");
     }
 }
-}
+
