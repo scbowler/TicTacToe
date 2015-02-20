7c03aab8b75d59c35d27951a3af74bb8a7a167dd
Scott Bowler <s.bowler1007@gmail.com>
2015-02-19 16:32:18 -0800

    JS update 3 early functionality Scott

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,11 +1,61 @@
-var boardArray, playerX, playerO, ties, whosTurn;
+var boardArray, playerX, playerO, ties, whosTurn, moves;
 
 function init(){
     boardArray = [['', '', ''], ['', '', ''], ['', '', '']];
     playerX = ["X", 0];
     playerO = ["O", 0];
     ties = 0;
-    whosTurn = "";
+    whosTurn = "x";
+    moves = 0;
+}
+
+function mainGame(ele){
+    ele.innerHTML = whosTurn;
+    
+    updateArray(ele);
+    
+    if(didWin(whosTurn)){
+        alert(whosTurn + "'s WON!")
+    }
+    
+    switchPlayer();
+}
+
+function updateArray(ele){
+    eleID = ele.getAttribute("id");
+    
+    switch (eleID) {
+            case "r1_c1":
+                boardArray[0][0] = whosTurn;
+                break;
+            case "r1_c2":
+                boardArray[0][1] = whosTurn;
+                break;
+            case "r1_c3":
+                boardArray[0][2] = whosTurn;
+                break;
+            case "r2_c1":
+                boardArray[1][0] = whosTurn;
+                break;
+            case "r2_c2":
+                boardArray[1][1] = whosTurn;
+                break;
+            case "r2_c3":
+                boardArray[1][2] = whosTurn;
+                break;
+            case "r3_c1":
+                boardArray[2][0] = whosTurn;
+                break;
+            case "r3_c2":
+                boardArray[2][1] = whosTurn;
+                break;
+            case "r3_c3":
+                boardArray[2][2] = whosTurn;
+                break;
+            default:
+                alert("There was a problem with the board array");
+                break;
+    }
 }
 
 
@@ -13,20 +63,20 @@ function didWin(player){
     var arrLength = boardArray.length;
     
     if(boardArray[0][0] == player && boardArray[1][1] == player && boardArray[2][2] == player){
-        return console.log("\\");
+        return "\\";
     }
     if(boardArray[0][2] == player && boardArray[1][1] == player && boardArray[2][0] == player){
-        return console.log("/");
+        return "/";
     }
     
     for(var i=0; i<arrLength; i++){
         if(boardArray[i][0] == player && boardArray[i][1] == player && boardArray[i][2] == player ){
             return console.log("r" + i);
         }else if(boardArray[0][i] == player && boardArray[1][i] == player && boardArray[2][i] == player ){
-            return console.log("c" + i);    
+            return "c" + i;    
         }
     }
-    return console.log(false);
+    return false;
 }
 
 function updateScore(player){
@@ -46,3 +96,14 @@ function updateScore(player){
     }
 }
 
+function switchPlayer(){
+    if(whosTurn == 'x'){
+        whosTurn = 'o';
+        return;
+    }
+    if(whosTurn == 'o'){
+        whosTurn = 'x';
+        return;
+    }
+    alert("Whos turn is it!?!?!?");
+}
