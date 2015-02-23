5d47592cb036b83621b420829db25d890f65a6fb
Scott Bowler <s.bowler1007@gmail.com>
2015-02-22 19:55:21 -0800

    First working version commit

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,5 +1,5 @@
 //set all global variable names
-var boardArray, playerX, playerO, ties, whosTurn, moves, oTurn, xTurn, boardSquares;
+var boardArray, playerX, playerO, ties, whosTurn, moves, oTurn, xTurn, boardSquares, gameState;
 
 //function called on page load to initialize all global variables
 function init(){
@@ -7,15 +7,20 @@ function init(){
     playerX = ["X", 0];                                      //array to hold info for the 'x' player contains: [player letter, number of wins]
     playerO = ["O", 0];                                      //array to hold info for the 'o' player contains: [player letter, number of wins]
     ties = 0;                                                //variable to hold the number of times there was a tie game
-    whosTurn = 'x';                                          //keeps track of whos turn it is using either 'x' or 'o'
+    whosTurn = '';                                          //keeps track of whos turn it is using either 'x' or 'o'
     moves = 0;                                               //number of turns taken for current game
     oTurn = document.getElementById('oCheck');
     xTurn = document.getElementById('xCheck');
     boardSquares = document.querySelectorAll('.squares');
-    var myString = "";
-    for(var i=0; i<boardSquares.length; i++){
-        boardSquares[i].innerHTML = i;
-    }
+    gameState = "over";
+}
+
+function startGame(){
+    resetGame();
+    gameState = "running";
+    
+    whosFirst();
+    alert(whosTurn + "'s go first!");
 }
 
 //function to randomly determin which player will go first / returns nothing
@@ -25,17 +30,30 @@ function whosFirst(){
     //if 'num' is equal to 1 'x' is assigned to 'whosTurn' so 'x' will be first 
     if(num === 1){
         whosTurn = playerX[0];
+        oTurn.removeAttribute("checked");
+        xTurn.setAttribute("checked", true);
     //if 'num' is not qual to 1 'o' is assigned to 'whosTurn' so 'o' will be first
     }else{
         whosTurn = playerO[0];
+        xTurn.removeAttribute("checked");
+        oTurn.setAttribute("checked", true);
     }
     
 }
 
 //this is the main game function that is called on a cell click / returns nothing / takes an html element as a param
 function mainGame(ele){
+    if(gameState == "over"){
+        if(confirm("Game Over:\nWould you like to start a new game?")){
+            startGame();
+            return;
+        }else{
+            return;
+        }
+    }
+    
     //if the element already has a 'x' or an 'o' exit the function and do nothing
-    if(ele.innerHTML == 'x' || ele.innerHTML == 'o'){
+    if(ele.innerHTML == 'X' || ele.innerHTML == 'O'){
         return; // exits the function
     }
     
@@ -48,25 +66,16 @@ function mainGame(ele){
     
     //calls the didWin() function to determine if a win condition exists
     var winner = didWin(whosTurn);
-    console.log(winner);
     if(winner){
+        gameState = "over";
         if(winner == "tie"){
-            alert("it was a tie!");
-            ties++;
-            resetGame();
+            alert("It is a tie!");
+            updateScore('t');
             return;
         }
         gameWon(winner);
         alert(whosTurn + "'s WON!") //do stuff if somone wins
-        if (whosTurn == 'x'){
-            playerX[1]++;
-            document.getElementById('xScore').innerHTML = playerX[1];
-            resetGame();
-        }else{
-            playerO[1]++;
-            document.getElementById('oScore').innerHTML = playerO[1];
-            resetGame();
-        }
+        updateScore(whosTurn);
         return;
     }
     
@@ -139,7 +148,6 @@ function didWin(player){
 
 function gameWon(line){
     var squareID;
-    var start = parseInt(line);
     
     switch (line){
         case "r0":
@@ -176,14 +184,17 @@ function gameWon(line){
 function updateScore(player){
     
     switch(player){
-            case "x":
+            case "X":
                 playerX[1]++;
+                document.getElementById('xScore').innerHTML += "l";
                 break;
-            case "o":
+            case "O":
                 playerO[1]++;
+                document.getElementById('oScore').innerHTML += "l";
                 break;
             case "t":
                 ties++;
+                document.getElementById('tScore').innerHTML += "l";
                 break;
             default:
                 alert("There was an error in the updateScore function");
@@ -191,14 +202,14 @@ function updateScore(player){
 }
 
 function switchPlayer(){
-    if(whosTurn == 'x'){
-        whosTurn = 'o';
+    if(whosTurn == 'X'){
+        whosTurn = 'O';
         xTurn.removeAttribute("checked");
         oTurn.setAttribute("checked", true);
         return;
     }
-    if(whosTurn == 'o'){
-        whosTurn = 'x';
+    if(whosTurn == 'O'){
+        whosTurn = 'X';
         oTurn.removeAttribute("checked");
         xTurn.setAttribute("checked", true);
         return;
@@ -210,17 +221,21 @@ function resetGame(){
     boardArray = [['','',''], ['','',''], ['','','']];
     moves = 0;
     
-    
-    
     var i = boardSquares.length;
-    while(i--) boardSquares[i].innerHTML = "";
+    while(i--) {
+        boardSquares[i].innerHTML = "";
+        boardSquares[i].classList.remove('win');
+    }
+    gameState = "running";
 }
 
 function resetAll(){
     resetGame();
     
     playerO[1] = 0;
+    document.getElementById('oScore').innerHTML = "";
     playerX[1] = 0;
-    
+    document.getElementById('xScore').innerHTML = "";
     ties = 0;
+    document.getElementById('tScore').innerHTML = "";
 }
